d57a2b7d1ef29c896e821bd563f2d28617562452
Pascal <nadasalsabilah_2428240136@mhs.mdp.ac.id>
2026-05-02 20:36:21 +0700

    update project

--- basic_laravel/resources/views/welcome.blade.php
+++ basic_laravel/resources/views/welcome.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite([])
-</head>
-<body>
-    <h1>Welcome Laravel</h1>
-
-    {{ $slot }}
-    
-</body>
-</html>
+<x-layout>
+    <h1>welcome</h1>
+    <a href="/Fakultas">List Fakultas</a>
+    <a href="/Fakultas-edit">Edit Fakultas</a>
+    <a href="/Fakultas-detail">Detail Fakultas</a>
+    </x-layo>
--- basic_laravel/routes/web.php
+++ basic_laravel/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Fakultas', function () {
+    return view('Fakultas.list-fakultas');
+});
+
+Route::get('/Fakultass', function () {
+    return view('Fakultas.edit-fakultas');
+});
+
+Route::get('/Fakultas-detail', function () {
+    return view('Fakultas.detail-fakultas');
+});
